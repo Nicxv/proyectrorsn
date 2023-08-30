5c33bcf27f11f2c4feb6f85686c1f4d6f5051b5d
Sim0n2o03 <107275068+Sim0n2o03@users.noreply.github.com>
2023-08-30 14:41:57 -0400

    a

--- src/app/pages/samsung1/samsung1.page.ts
+++ src/app/pages/samsung1/samsung1.page.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import type { IonModal } from '@ionic/angular';
-import { AnimationController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-samsung1',
@@ -9,46 +7,12 @@ import { AnimationController } from '@ionic/angular';
   styleUrls: ['./samsung1.page.scss'],
 })
 export class Samsung1Page implements OnInit {
-  @ViewChild('modal', { static: true }) modal!: IonModal;
+  
 
-  constructor(private animationCtrl: AnimationController, private root: Root) { }
+  constructor() { }
 
   ngOnInit() {
-      const enterAnimation = (baseEl: HTMLElement) => {
-      const root = baseEl.shadowRoot;
-
-      const backdropAnimation = this.animationCtrl
-        .create()
-        .addElement(this.root.querySelector('ion-backdrop'))
-        .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
-
-      const wrapperAnimation = this.animationCtrl
-        .create()
-        .addElement(this.root.querySelector('.modal-wrapper'))
-        .keyframes([
-          { offset: 0, opacity: '0', transform: 'scale(0)' },
-          { offset: 1, opacity: '0.99', transform: 'scale(1)' },
-        ]);
-
-      return this.animationCtrl
-        .create()
-        .addElement(baseEl)
-        .easing('ease-out')
-        .duration(500)
-        .addAnimation([backdropAnimation, wrapperAnimation]);
-    };
-
-    const leaveAnimation = (baseEl: HTMLElement) => {
-      return enterAnimation(baseEl).direction('reverse');
-    };
-
-    this.modal.enterAnimation = enterAnimation;
-    this.modal.leaveAnimation = leaveAnimation;
-  }
-
-  closeModal() {
-    this.modal.dismiss();
-  }
+      
 }
   
 
